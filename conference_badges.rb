8e1051a08cefb01289cb6feb03b756ab136238f8
Lauren Days <lmchambers0420@gmail.com>
2018-11-18 23:23:42 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,3 +14,13 @@ def assign_rooms(attendees)
   attendees.each_with_index.map do |name, index| "Hello, #{name}! You'll be assigned to room #{index + 1}!"
 end
 end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+    end
+    
+  assign_rooms(attendees).each do |assignment|
+    puts assignment
+    end
+end
