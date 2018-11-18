13b6992ea45574ebda0e8b5ca95a3c219339e126
Lauren Days <lmchambers0420@gmail.com>
2018-11-18 23:15:44 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
 # Write your code here.
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.map do |name| "Hello, my name is #{name}."
+end
+end
+
+def assign_rooms(attendees)
+  
+end
